519d574d6984130dd5fe1c9a61995d76857a4ff9
aangyeoki <v.angelica317@gmail.com>
2025-10-20 01:50:35 +0700

    udh smpe introduction

---
+++ src/styles.js
@@ -0,0 +1,18 @@
+const styles = {
+  paddingX: "sm:px-16 px-6",
+  paddingY: "sm:py-16 py-6",
+  padding: "sm:px-16 px-6 sm:py-16 py-10",
+
+heroHeadText:
+  "font-black text-white lg:text-[48px] sm:text-[36px] xs:text-[30px] text-[24px] lg:leading-[56px] mt-2",
+
+heroSubText:
+  "text-[#dfd9ff] font-medium lg:text-[20px] sm:text-[16px] xs:text-[14px] text-[12px] lg:leading-[24px]",
+
+  sectionHeadText:
+    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+  sectionSubText:
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+};
+
+export { styles };
